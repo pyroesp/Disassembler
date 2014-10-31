9f96195f60344d5753c52bd8d622fefd5af82c7b
pyroesp <nicolaslopezesp@gmail.com>
2014-10-31 15:05:42 +0100

    Added a bit of code to write to a code.asm file

--- src/main.c
+++ src/main.c
@@ -8,6 +8,9 @@
  *      This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 **/
 
+// config/CHIP-8.csv program/SpaceInvaders.ch8
+// config/CHIP-8.csv program/Maze.ch8
+
 #include "disasm.h"
 #include "config.h"
 #include "program.h"
@@ -28,9 +31,18 @@ int main(int argc, char *argv[])
     program_GetList(&code, &code.jumpList, PROGRAM_INSTR_TYPE_JUMP_ABS, "label_%03d", PROGRAM_LBL_LEN); // jump
     program_GetList(&code, &code.callList, PROGRAM_INSTR_TYPE_CALL, "function_%03d", PROGRAM_FUNC_LEN); // call
 
-    disasm_GenerateOutput(&code, DISASM_ARG_DEC, DISASM_ADD_ADDR | DISASM_ADD_OPCODE);
+    disasm_GenerateOutput(&code, DISASM_ARG_HEX, DISASM_ADD_ADDR | DISASM_ADD_OPCODE);
 
-    printf("%s", code.outputASM[0]);
+    FILE *fp;
+    fp = fopen("output/code.asm", "w");
+
+    if (fp != NULL)
+    {
+        fprintf(fp, "%s", code.outputASM[0]);
+        fclose(fp);
+    }
+
+    //printf("%s", code.outputASM[0]);
 
     disasm_Free(&code);
     return 0;
